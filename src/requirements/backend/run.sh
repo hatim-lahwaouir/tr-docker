91e5d5b8b6580187cc56657644abff2e9c9edb65
miloki <hatimh351@gmail.com>
2024-09-20 15:03:43 +0100

    v1

--- src/requirements/backend/run.sh
+++ src/requirements/backend/run.sh
@@ -2,11 +2,18 @@
 cd /app/backend/
 
 
+
+echo "Waiting for postgres..."
+
+while ! nc -z $SQL_HOST $SQL_PORT; do
+    sleep 0.1
+done
+echo "PostgreSQL started"
+
+
+
 python3 manage.py makemigrations
-python3 manage.py makemigrations Models
 python3 manage.py migrate
-python3 manage.py migrate Models
-python manage.py showmigrations
 
 
 exec python3 manage.py runserver 0.0.0.0:8000
